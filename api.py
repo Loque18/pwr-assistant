b446ca4b179843f0de63b4ff74abd4ad43c47844
loque18 <madridx2017@gmail.com>
2025-03-17 16:15:24 -0500

    feat: changed endpoint

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from responses.rag_res import RagResponse
 
 from dto.llm_dto import LlmChatDto
 
-API_URL = "http://209.38.157.244:8000"
+API_URL = "http://143.244.181.211:8000"
 
 api = {
     'rag': {
